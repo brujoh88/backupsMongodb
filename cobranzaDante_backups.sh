dbdfdeb18494c3c7dc5ff33fcb244c827c59b418
Gus - MVC <gustavotiseira@gmail>
2023-06-08 10:23:43 -0300

    correcion en el nombre del archivo | ej tarea automatica crontab

--- cobranzaDante_backups.sh
+++ cobranzaDante_backups.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Obtener la fecha y hora actual
-current_date=$(date +"%d-%m-%Y_%H-%M-%S")
+current_date=$(date +"%d-%m-%Y_%H-%M")
 
 # Paso 1: Levantar el contenedor de MongoDB
 docker run --name mongo_backup -d mongo
@@ -18,9 +18,13 @@ docker exec -i mongo_backup mongodump --config /config.conf
 # Eliminar el archivo de configuración
 rm config.conf
 
-# Paso 3: Copiar la carpeta de backup a la máquina host
-docker cp mongo_backup:/dump ./backup_$current_date
+# Paso 3: Copiar la carpeta de backup a la máquina host (la maquina host debe tener creada la capeta dentro de la carpeta personal backups_Cobranza)
+docker cp mongo_backup:/dump ./backups_Cobranza/backup_$current_date
 
 # Paso 4: Destruir el contenedor
 docker stop mongo_backup
 docker rm mongo_backup
+
+# Paso 5: Automatizar tarea
+#ej: cada 5 min
+# */5 * * * * /bin/bash /ruta/del/archivo/script.sh
